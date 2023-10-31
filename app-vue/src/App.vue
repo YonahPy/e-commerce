7299994705605f95766d6a3dd4aef8110d20f295
YonahPy <jrjonas487@gmail.com>
2023-10-31 17:59:44 -0300

    dependências instaladas

---
+++ app-vue/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  
+<p>ola meu nome é</p>
+  
+</template>
+
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<style >
+p{
+  color: green;
+  background-color: red;
+}
+</style>
