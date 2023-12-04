c8340685f8667e2a500771baa7ab900c306f93df
YonahPy <jrjonas487@gmail.com>
2023-12-04 16:00:38 -0300

    add requirements

--- core/core/settings.py
+++ core/core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_b*ardui=jdm6r0cq$$q3pa%i7nl7vydb@(gmkkky^e^94e+l*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
