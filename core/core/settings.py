911ecd081b9f4fb2ce7dd32013bc8a7673966c4a
YonahPy <jrjonas487@gmail.com>
2023-12-10 12:35:42 -0300

    .

--- core/core/settings.py
+++ core/core/settings.py
@@ -140,8 +140,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+import os
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 
--- core/core/urls.py
+++ core/core/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +9,4 @@ urlpatterns = [
     path('api-auth/', include('authentication.urls')),
     path('api/checkout/', include('payment.urls')),
     
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
